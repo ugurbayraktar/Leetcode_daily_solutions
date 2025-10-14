3fec77498563f93c44a18beafa9bb71c59a41829
ugurbayraktar <61114676+ugurbayraktar@users.noreply.github.com>
2025-10-14 21:29:00 +0300

    question 1748

---
+++ src/easy/sumOfUniqueElements.java
@@ -0,0 +1,20 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class sumOfUniqueElements {
+    public int sumOfUnique(int[] nums) {
+        //  1,2,3,2
+
+        HashMap<Integer , Integer> map = new HashMap<>();
+        int sum=0;
+
+        for(int number : nums){
+            //if(!set.contains(number)) sum= sum+ number;
+        }
+
+
+        return 5;
+    }
+}
