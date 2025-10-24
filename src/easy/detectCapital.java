7a0eefc11be0ac6b99baa6be3572d8d0b4e94996
ugurbayraktar <61114676+ugurbayraktar@users.noreply.github.com>
2025-10-24 08:20:43 +0300

    question 528

--- src/easy/detectCapital.java
+++ src/easy/detectCapital.java
@@ -1,8 +1,35 @@
 package easy;
 
+import java.net.UnixDomainSocketAddress;
+
 public class detectCapital {
     public boolean detectCapitalUse(String word) {
 
-        return true;
+        return word.equals(word.toUpperCase())  ||
+                word.equals(word.toLowerCase()) ||
+                word.equals(Character.toUpperCase(word.charAt(0)) + word.substring(1).toLowerCase());
+
     }
 }
+
+/* V1
+
+        for(int i = 1 ; i<word.length(); i ++){
+            char c = word.charAt(i);
+
+            if(Character.isUpperCase(word.charAt(0))){
+                if(Character.isUpperCase(word.charAt(1))){
+                    if(!Character.isUpperCase(word.charAt(i))) return false;
+                }else{
+                    if(!Character.isLowerCase(word.charAt(i))) return false;
+                }
+
+            }else{
+                if(Character.isUpperCase(word.charAt(i))){
+                    return false;
+                }
+
+            }
+        }
+        return true;
+ */
