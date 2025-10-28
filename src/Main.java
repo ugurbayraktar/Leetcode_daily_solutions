8e8bbc0a6b64892d2af304903c3a741eccd4c504
ugurbayraktar <61114676+ugurbayraktar@users.noreply.github.com>
2025-10-28 17:27:55 +0100

    question 1480

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import easy.countNumberofConsStrings;
+import easy.countNegativeInSortedMatrix;
 
 import java.util.Arrays;
 //In order to use the solution, please change the related class to wished one and edit the entry parameter according to that question.
@@ -6,21 +6,22 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        countNumberofConsStrings solver = new countNumberofConsStrings();
+        countNegativeInSortedMatrix solver = new countNegativeInSortedMatrix();
         //Example entry parameters
-        int[] question = {4000,3000,1000,2000};
-        int[][] questionDoubleArray = {{1,2,3},{3,2,1}};
+        int[] question = {1,2,2,1};
+        int[] question2 = {2,2};
+        int[][] questionDoubleArray = {{4,3,2,-1},{3,2,1,-1},{1,1,-1,-2},{-1,-1,-2,-3}};
         int low = 234;
         int heigh = 716760;
         double d = 36.50;
-        String[] words = {"a","b","c","ab","ac","bc","abc"};
-        String s = "Leetcode, daily. streak- achieved";
+        String[] words = {"test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"};
+        String s = "sAa";
         String t  = "ab";
 
 
         timer.measure(() -> { //Timer function to follow process time.
 
-            int result = solver.countConsistentStrings(t, words); //Careful about the return type.
+            int result = solver.countNegatives(questionDoubleArray); //Careful about the return type.
             System.out.println("Sonuç: " + result);
 
         });
