7d09793fe73aefe8efff75c5fbfd04ac7eeadf5f
ugurbayraktar <61114676+ugurbayraktar@users.noreply.github.com>
2025-11-02 16:45:44 +0100

    question 1480

--- src/easy/runningSumOf1DArray.java
+++ src/easy/runningSumOf1DArray.java
@@ -1,8 +1,26 @@
 package easy;
 
+import java.util.stream.IntStream;
+
 public class runningSumOf1DArray {
     public int[] runningSum(int[] nums) {
 
-        return null;
+        int[] result= new int[1];
+
+        int[] runningSum = IntStream.of(nums)
+                .map(n -> result[0]+=n)
+                .toArray();
+
+        return runningSum;
     }
 }
+
+/*V1
+ int counter=0;
+        int[] result= new int[nums.length];
+
+        for(int i =0; i< nums.length; i++){
+            counter = counter + nums[i];
+            result[i]= counter;
+        }
+*/
